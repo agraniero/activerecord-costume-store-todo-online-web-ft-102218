aaa3056254878d7b5d17dae5af16c4fa28854cab
Anthony Graniero <ananfro@gmail.com>
2019-01-16 16:10:42 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -4,7 +4,7 @@ class CreateCostumeStores < ActiveRecord::Migration[5.1]
       t.string :name
       t.string :location
       t.integer :constume_inventory
-      t.integer :employees
+      t.integer :num_of_employees
       t.timestamps
     end
   end
