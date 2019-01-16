3b0aa33acffb194df33313f201c86698ff98d611
Anthony Graniero <ananfro@gmail.com>
2019-01-16 16:16:01 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -6,7 +6,7 @@ class CreateCostumeStores < ActiveRecord::Migration[5.1]
       t.integer :constume_inventory
       t.integer :num_of_employees
       t.boolean :still_in_business
-      t.datetime     
+      t.datetime :start_time   
     end
   end
 end
